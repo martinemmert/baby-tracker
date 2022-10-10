fb8ecb6e18b8960dba5ed3e65f2c7ea668419a8d
Martin Emmert <martin@martinemmert.de>
2022-10-11 00:45:26 +0200

    :sparkles: Make PWA ready

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [svelte(), VitePWA({
+    devOptions: {
+      enabled: true
+    },
+    includeAssets: ["./public/*.ico", "./public/*.png", "./public/*.svg"],
+    manifest: {
+      "name": "Baby Tracker",
+      "short_name": "BabyTracker",
+      "description": "Helps remembering the last time you nursed your little one.",
+      "icons": [
+        {
+          "src": "/assets/android-chrome-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png",
+        },
+        {
+          "src": "/assets/maskable_icon_x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ],
+      "theme_color": "#1e293b",
+      "background_color": "#1e293b",
+      "display": "standalone"
+    }
+  })]
+});
